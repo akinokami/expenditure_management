afd8df1268dbb233d66f10e377afd523d113a517
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-08-02 12:25:57 +0800

    a

---
+++ lib/views/statistical/statistical_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StatisticalScreen extends StatelessWidget {
+  const StatisticalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Thống kê'),
+      ),
+      body: Center(
+        child: Text('Thống kê'),
+      ),
+    );
+  }
+}
