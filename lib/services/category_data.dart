1ee1c4b1641d0e29a5b6d1ecca5184a078f0cb45
HWSEO-AKINOKAMI-53183 <hwseo-akinokami-53183@admindeMac-mini.local>
2024-08-03 13:09:24 +0800

    a

---
+++ lib/services/category_data.dart
@@ -0,0 +1,24 @@
+import 'package:expenditure_management/models/expenditure_model.dart';
+
+List<CategoryModel> categoryList = [
+  CategoryModel(id: 1, name: "Eat and Drink", nameVn: "Ăn uống"),
+  CategoryModel(id: 2, name: "Shopping", nameVn: "Di chuyển"),
+  CategoryModel(id: 3, name: "Travel", nameVn: "Di chuyển"),
+  CategoryModel(id: 4, name: "Entertainment", nameVn: "Di chuyển"),
+  CategoryModel(id: 5, name: "Education", nameVn: "Di chuyển"),
+  CategoryModel(id: 6, name: "Beauty", nameVn: "Di chuyển"),
+  CategoryModel(id: 7, name: "Car", nameVn: "Di chuyển"),
+  CategoryModel(id: 8, name: "Alcohol", nameVn: "Di chuyển"),
+  CategoryModel(id: 9, name: "Cigarette", nameVn: "Di chuyển"),
+  CategoryModel(id: 10, name: "Electronic device", nameVn: "Di chuyển"),
+  CategoryModel(id: 11, name: "Health", nameVn: "Ăn uống"),
+  CategoryModel(id: 12, name: "Repair", nameVn: "Di chuyển"),
+  CategoryModel(id: 13, name: "House", nameVn: "Di chuyển"),
+  CategoryModel(id: 14, name: "Present", nameVn: "Di chuyển"),
+  CategoryModel(id: 15, name: "Donations", nameVn: "Di chuyển"),
+  CategoryModel(id: 16, name: "Lottery", nameVn: "Di chuyển"),
+  CategoryModel(id: 17, name: "Children", nameVn: "Di chuyển"),
+  CategoryModel(id: 18, name: "Loan", nameVn: "Di chuyển"),
+  CategoryModel(id: 19, name: "Salary", nameVn: "Di chuyển"),
+  CategoryModel(id: 20, name: "Bonus", nameVn: "Di chuyển"),
+];
